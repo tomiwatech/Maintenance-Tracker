bf4bd0cfc33def1718aa8bbafd95c637d44bb8fe
Sanni Michael Tomiwa <tomiwatech@gmail.com>
2018-05-17 14:55:01 +0100

    [#157645901] Added .travis.yml

--- server/tests/controllers/user.js
+++ server/tests/controllers/user.js
@@ -1,7 +1,6 @@
 import chai from 'chai';
 import chaiHttp from 'chai-http';
 import server from '../../index';
-import UserController from '../../controllers/userController';
 
 chai.use(chaiHttp);
 chai.should();
